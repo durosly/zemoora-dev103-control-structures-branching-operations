eb53064621a4baee60ffe680b559427a028fced0
durosly <slyboydon1@gmail.com>
2023-09-27 12:05:18 +0100

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,95 @@
+/*
+const score = 56;
+
+if (score >= 80) {
+	console.log("Excellent");
+} else if (score >= 70) {
+	console.log("Very Good");
+} else if (score >= 60) {
+	console.log("Good");
+} else if (score >= 50) {
+	console.log("Average");
+} else if (score >= 40) {
+	console.log("Poor");
+} else if (score >= 30) {
+	console.log("Very Poor");
+} else {
+	console.log("failed");
+}
+
+const yearOfBirth = 1962;
+
+yearOfBirth >= 2000 ? console.log("New era") : console.log("Old era");
+
+const age = 2023 - yearOfBirth;
+
+const message = age > 18 ? "Off age" : "Still young";
+console.log(message);
+*/
+
+// write a program to checks two
+// numbers and display whether or not they are equal
+
+/* const num1 = 5;
+const num2 = 6;
+
+if (num1 === num2) {
+	console.log("Numbers are equal");
+} else {
+	console.log("Numbers are not equal");
+} */
+
+// write a program to check whether a number odd or even
+/* const num = 5
+
+if(num % 2 === 0) {
+    console.log("Number is even")
+} else {
+    console.log("Number is odd")
+} */
+
+// write a program that checks if a number is positive or negative
+
+/* const num = -6;
+
+if (num > 0) {
+	console.log("Number is positive");
+} else {
+	console.log("Number is negative");
+} */
+
+// write a program that checks if a year is leap year
+
+/* const year = 2020;
+
+if (year % 400 === 0) {
+	console.log("Leap year");
+} else if (year % 100 === 0) {
+	console.log("Not a leap year");
+} else if (year % 4 === 0) {
+	console.log("Leap year");
+} else {
+	console.log("Not a leap year");
+} */
+
+// write a program to determine whether a person is eligible to vote
+/* const age = 16;
+
+if (age >= 18) {
+	console.log("You are eligible to vote");
+} else {
+	console.log("you are not eligible to vote");
+} */
+
+// write a program that sets the value of n to be 1 when m
+// is positive or -1 when m is negative
+
+/* const m = 6;
+
+const n = m > 0 ? 1 : -1;
+console.log(n); */
+
+/* 
+    ⚠️Task
+    study about loops in javascript
+*/
